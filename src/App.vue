a90637540794da84c7833906ed8637a160c3b7e3
Emil Österlund <emil@larsemil.se>
2024-09-02 21:36:15 +0200

    Hello world

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <h1>Nyktra samkväm</h1>
+    <img class="party" src="/bild.webp">
+    <p>Det är klart att det går att festa nyktert. Ett alkoholfritt samkväm kan på många sätt överglänsa en fest som slutar i <i>alkoångest</i>.</p>
+    <p>Vill du också vara en del av den nyktra gemenskapen? </p>
+    <p><strong>Håll utkik efter nyktra samkväm på en plats nära dig!</strong></p>
+  </div>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Quattrocento;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1,
+h2,
+h3 {
+  font-family: Oswald;
+  font-weight: bold;
+}
+
+.wrapper{
+  width: 500px; 
+  margin: 0 auto;
+}
+
+img.party{
+  width:400px;
 }
 </style>
